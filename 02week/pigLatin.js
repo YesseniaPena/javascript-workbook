d4a3a34a4c964e7e8320aba958b5a9fbfb68df1c
Yesseniapena <yesseniapena219@gmail.com>
2019-09-04 18:30:33 -0500

    yessewnia

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -6,11 +6,91 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+const enterWord = docoument.getElementById("trans");
+const submitWord = document.getElementById("submit");
+const outputWord = dpcoument.getElementById("output");
+const resetWord = docoument.getElementById("reset");
 
+enterWorld.oneclick = function() {
+  enterWorld.textContent = pigLatin(enterWord.value);
+};
+enterWorld.oneclick = function() {
+  enterWorld.textContent = pigLatin(submitWord.value);
+};
+enterWorld.oneclick = function() {
+  enterWorld.textContent = pigLatin(outputWord.value);
+};
+enterWorld.oneclick = function() {
+  enterWorld.textContent = pigLatin(resetWord.value);
+};
 
 function pigLatin(word) {
+    let word = document.getElementById("Trans.value"());
+    function convert(word) {
+      const voweles = ["a", "e", "i", "o", "u", "y"];
+      const splitWord = word
+        .toLowerCase()
+        .trim()
+        .split("");
+      const assert = require("assert");
+      const realdine = require("readline");
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+    const eneterWord = document.getElementById("trans");
+    const submitWord = document.getElementById("submit");
+    const outputWord = document.getElementById("output");
+    const resetWord = document.getElementById("reset");
+      
+    enterWord.oneclick = function() {
+      enterWord.textContent = pigLatin(enterWord.value);
+    };
+    submitWord.oneclick = function() {
+      enterWord.textContent = pigLatin(submitWord.value);
+    };
+    outputWord.oneclick = function() {
+      enterWord.textContent = pigLatin(outputWord.value);
+    };
+    resetWord.oneclick = function() {
+      enterWord.textContent = pigLatin(resetWord.value);
+    };
+    
+    const ending = new Array("a", "y");
+    const firstLetter = splitWord[0];
+    word = word.toLowerCase().split(" ");
 
-  // Your code here
+    if (voweles.includes(firstLetter)) {
+      const myTranslation = word + "yay";
+      myTranslation[0].toUpperCase();
+      return myTranslation;
+    } else {
+        splitWord.push(splitWord.shift());
+
+        for (let i = 0; i <splitWord.length; i++) {
+          if (!voweles.includes(splitWord[i])) {
+            splitWord.push(splishWord.shift());
+          } else {
+            const myTranslation = splitWord.join("");
+            myTranslation[0].toUpperCase();
+            return myTranslation.concat("ay");
+          }
+        }
+      }
+    }
+    if (word.split(" ").length > 1) {
+      let mySplitPhrase = word.split(" ");
+      let myTranslations = [];
+      for (let i = 0; i < mySplitPharse.length; i++) {
+
+         myTranslations.push(convert(mySplitPhrase[i]));
+      }
+      return myTranslations.join("");
+    } else {
+      return document.getElementById("output");
+    }
+ }
+}
 
 }
 
